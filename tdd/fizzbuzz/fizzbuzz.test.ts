aaf9960e73edb80bf1790d058c966cd07f6d148c
Xavier <xavier.huberdeau@ilek.fr>
2022-09-02 23:30:28 +0200

    Refactor tests name

--- tdd/fizzbuzz/fizzbuzz.test.ts
+++ tdd/fizzbuzz/fizzbuzz.test.ts
@@ -1,11 +1,11 @@
 import fizzbuzz from "./fizzbuzz";
 
 describe("FizzBuzz", () => {
-  it("Should return 1 for 1", () => {
+  it("Returns 1 for 1", () => {
     expect(fizzbuzz(1)).toEqual(1);
   });
 
-  it("Should return 2 for 2", () => {
+  it("Returns 2 for 2", () => {
     expect(fizzbuzz(2)).toEqual(2);
   });
 });
