dd08a2b8aff60f71a531b7b5673cbe3da567dcb8
Xavier <xavier.huberdeau@ilek.fr>
2022-09-04 14:42:45 +0200

    Pass 2018 test case

--- tdd/leapYear/leapYears.ts
+++ tdd/leapYear/leapYears.ts
@@ -1,10 +1,11 @@
 const leapYears = (year: number) => {
   if (isDivisbleBy100(year) && !isDivisbleBy400(year)) return false;
-  if (year === 2017) return false;
+  if (!isDivisbleBy4(year)) return false;
 
   return true;
 };
 
+const isDivisbleBy4 = (value: number) => divisible(value, 4);
 const isDivisbleBy400 = (value: number) => divisible(value, 400);
 const isDivisbleBy100 = (value: number) => divisible(value, 100);
 const divisible = (value: number, divider: number) => value % divider === 0;
