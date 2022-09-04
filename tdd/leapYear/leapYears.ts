086e0dcbc4bc8d63f56e151af8c007cd819f1016
Xavier <xavier.huberdeau@ilek.fr>
2022-09-04 14:51:31 +0200

    Refactor typo and expressiveness

--- tdd/leapYear/leapYears.ts
+++ tdd/leapYear/leapYears.ts
@@ -1,13 +1,13 @@
 const leapYears = (year: number) => {
-  if (isDivisbleBy100(year) && !isDivisbleBy400(year)) return false;
-  if (!isDivisbleBy4(year)) return false;
+  if (isDivisibleBy100(year) && !isDivisibleBy400(year)) return false;
+  if (!isDivisibleBy4(year)) return false;
 
   return true;
 };
 
-const isDivisbleBy4 = (value: number) => divisible(value, 4);
-const isDivisbleBy400 = (value: number) => divisible(value, 400);
-const isDivisbleBy100 = (value: number) => divisible(value, 100);
-const divisible = (value: number, divider: number) => value % divider === 0;
+const isDivisibleBy4 = (value: number) => isDivisible(value, 4);
+const isDivisibleBy400 = (value: number) => isDivisible(value, 400);
+const isDivisibleBy100 = (value: number) => isDivisible(value, 100);
+const isDivisible = (value: number, divider: number) => value % divider === 0;
 
 export default leapYears;
