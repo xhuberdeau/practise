b00a370c377ab6f1d0b3fff9fb4fc4ec53c527be
Xavier <xavier.huberdeau@ilek.fr>
2022-09-04 15:55:15 +0200

    Refactor tests structure

--- tdd/stringCalculator/stringCalculator.test.ts
+++ tdd/stringCalculator/stringCalculator.test.ts
@@ -7,19 +7,21 @@ describe("String calculator", () => {
     stringCalculator = new StringCalculator();
   });
 
-  test('returns "0" for ""', () => {
-    expect(stringCalculator.add("")).toEqual("0");
-  });
+  describe("Basic sum with one string param", () => {
+    test('returns "0" for ""', () => {
+      expect(stringCalculator.add("")).toEqual("0");
+    });
 
-  test('returns "1" for "1"', () => {
-    expect(stringCalculator.add("1")).toEqual("1");
-  });
+    test('returns "1" for "1"', () => {
+      expect(stringCalculator.add("1")).toEqual("1");
+    });
 
-  test('returns "3" for "1,2"', () => {
-    expect(stringCalculator.add("1,2")).toEqual("3");
-  });
+    test('returns "3" for "1,2"', () => {
+      expect(stringCalculator.add("1,2")).toEqual("3");
+    });
 
-  test('returns "3.3" for "1.1,2.2"', () => {
-    expect(stringCalculator.add("1.1,2.2")).toEqual("3.3");
+    test('returns "3.3" for "1.1,2.2"', () => {
+      expect(stringCalculator.add("1.1,2.2")).toEqual("3.3");
+    });
   });
 });
