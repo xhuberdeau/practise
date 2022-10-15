945997053290836f315bed7c6d4a226dabed9bb2
Xavier <xavier.huberdeau@ilek.fr>
2022-10-15 19:04:17 +0200

    Create dev and test files when creating the kata on filesystem

--- tooling/tdd/kataImporter/adapter/fileSystemKataSaver.ts
+++ tooling/tdd/kataImporter/adapter/fileSystemKataSaver.ts
@@ -7,16 +7,40 @@ const TDD_DIR_PATH = "./tdd/";
 
 class FileSystemKataSaver implements KataSaver {
   public async saveKata(kata: Kata): Promise<string> {
-    const kataPath = `${TDD_DIR_PATH}${toCamelCase(kata.name)}`;
+    await this.createKataFolder(kata);
+    const readmeLocalPath = await this.createKataReadme(kata);
+    await this.initializeKataDevFiles(kata);
+    return readmeLocalPath;
+  }
 
-    await mkdir(kataPath);
+  private async createKataFolder(kata: Kata): Promise<void> {
+    await mkdir(this.getKataFolderPath(kata));
+  }
+
+  private async createKataReadme(kata: Kata): Promise<string> {
     const readmeRequest = await fetch(kata.URL);
     const readme = await readmeRequest.blob();
-    const readmeLocalPath = `${kataPath}/README.md`;
-    console.log(readmeLocalPath, kataPath, kata);
+    const readmeLocalPath = `${this.getKataFolderPath(kata)}/README.md`;
     await writeFile(readmeLocalPath, await readme.text());
 
     return readmeLocalPath;
+  }
+
+  private async initializeKataDevFiles(kata: Kata) {
+    const devFilePath = `${this.getKataFolderPath(kata)}/${toCamelCase(
+      kata.name
+    )}.ts`;
+    await writeFile(devFilePath, "");
+
+    const testFilePath = `${this.getKataFolderPath(kata)}/${toCamelCase(
+      kata.name
+    )}.test.ts`;
+
+    await writeFile(testFilePath, "");
+  }
+
+  private getKataFolderPath(kata): string {
+    return `${TDD_DIR_PATH}${toCamelCase(kata.name)}`;
   }
 }
 
